edf8610fa55146e5eac5cbeddcdb3c6106cecc2e
Andevc <escobarhandres@gmail.com>
2023-08-18 23:37:24 -0400

    Add Content

--- 2.Remember-CSS/10_Practicas-CSS/Newsletter1/script.js
+++
@@ -1,6 +0,0 @@
-const btnSuscribe = document.getElementById('suscribe');
-const btnMessage = document.getElementById('btnMessage');
-
-document.getElementById('suscribe').addEventListener('click', function(){
-     document.getElementById('message').style.overflow = 'hidden';
-})
